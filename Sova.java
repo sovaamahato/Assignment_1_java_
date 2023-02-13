b5dc48e5e217a8eda0d8d81ac15c3af963564ea7
sova <mahatoshobha143@gmail.com>
2023-02-13 23:09:52 +0545

    task completed

---
+++ Sova.java
@@ -0,0 +1,19 @@
+public class Sova{
+    public static void main(String[] args){
+        System.out.println("Name of the organization: Oraget street food\n\n");
+        System.out.println("Address:");
+        System.out.println("----state: KM");
+        System.out.println("----Zip Code: 2304");
+        System.out.println("----Country: Nepal\n\n");
+        System.out.println("Contact Information:");
+        System.out.println("----Mobile Number: 9804391172");
+        System.out.println("----Email: oraget.rnb@gmail.com");
+        System.out.println("");
+
+
+        
+        
+
+
+    }
+}
